f4e2e0360053dd01ce559e9a63bfb96cc7c02f63
jack <os-shenxj@jsfund.cn>
2019-07-22 00:27:32 +0800

    feat: 学习生命周期方法

--- src/app1/index.js
+++ src/app1/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
 import Root from './App';
+import { unloadApplication } from 'single-spa';
 
 const reactLifecycles = singleSpaReact({
   React,
@@ -10,16 +11,28 @@ const reactLifecycles = singleSpaReact({
   domElementGetter,
 });
 
+// This lifecycle function will be called once, right before the registered application is mounted for the first time.
 export function bootstrap(props) {
+  console.debug('react app bootstrap', props);
   return reactLifecycles.bootstrap(props);
 }
 
 export function mount(props) {
+  console.debug('react app mount', props);
   return reactLifecycles.mount(props);
 }
 
 export function unmount(props) {
+  console.debug('react app unmount', props);
+  unloadApplication('app-1'); // 卸载react app,会触发unload生命周期
   return reactLifecycles.unmount(props);
+}
+
+export function unload(props) {
+  return Promise.resolve().then(() => {
+    // Hot-reloading implementation goes here
+    console.log('react app unloaded!');
+  });
 }
 
 function domElementGetter() {
--- src/root-app/index.jsx
+++ src/root-app/index.jsx
@@ -25,8 +25,18 @@ class App extends React.Component {
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerApplication('app-1', () => import('../app1/index.js'), pathPrefix('/app1'));
-registerApplication('app-2', () => import('../app2/index.js'), pathPrefix('/app2'));
+/**
+ * 第四个参数为自定义属性 https://single-spa.js.org/docs/building-applications.html
+ share a common access token with all child apps
+ pass down some initialization information, like the rendering target
+ pass a reference to a common event bus so each app may talk to each other
+ */
+registerApplication('app-1', () => import('../app1/index.js'), pathPrefix('/app1'), {
+  appInfo: 'react app',
+});
+registerApplication('app-2', () => import('../app2/index.js'), pathPrefix('/app2'), {
+  appInfo: 'angular app',
+});
 
 start();
 
