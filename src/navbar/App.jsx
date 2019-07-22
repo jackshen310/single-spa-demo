021bcc5c297af904e1a2005d88418f8bbd37b5fb
jack <os-shenxj@jsfund.cn>
2019-07-22 15:52:58 +0800

    feat: 将导航页重构为single-spa应用

---
+++ src/navbar/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+require('./index.css');
+
+class App extends React.Component {
+  handleRoute = path => {
+    singleSpaNavigate(path);
+  };
+  render() {
+    return (
+      <ul className="navbar">
+        <a onClick={this.handleRoute.bind(this, '/app1')}>
+          <li>App 1</li>
+        </a>
+        <a onClick={this.handleRoute.bind(this, '/app2')}>
+          <li>App 2</li>
+        </a>
+        <a onClick={this.handleRoute.bind(this, '/app3')}>
+          <li>App 3</li>
+        </a>
+      </ul>
+    );
+  }
+}
+
+export default App;
--- src/root-app/index.jsx
+++ src/root-app/index.jsx
@@ -1,29 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { registerApplication, start } from 'single-spa';
-require('./index.css');
-// 或者指定loader http://webpack.wuhaolin.cn/1%E5%85%A5%E9%97%A8/1-4%E4%BD%BF%E7%94%A8Loader.html
-// require('style-loader!css-loader?minimize!./index.css');
-
-class App extends React.Component {
-  handleRoute = path => {
-    singleSpaNavigate(path);
-  };
-  render() {
-    return (
-      <ul className="navbar">
-        <a onClick={this.handleRoute.bind(this, '/app1')}>
-          <li>App 1</li>
-        </a>
-        <a onClick={this.handleRoute.bind(this, '/app2')}>
-          <li>App 2</li>
-        </a>
-      </ul>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 /**
  * 第四个参数为自定义属性 https://single-spa.js.org/docs/building-applications.html
@@ -31,6 +6,14 @@ ReactDOM.render(<App />, document.getElementById('root'));
  pass down some initialization information, like the rendering target
  pass a reference to a common event bus so each app may talk to each other
  */
+// 导航页
+registerApplication(
+  'navbar',
+  () => import('../navbar/index.js'),
+  () => {
+    return () => true; // 导航页，永远显示
+  }
+);
 registerApplication('app-1', () => import('../app1/index.js'), pathPrefix('/app1'), {
   appInfo: 'react app',
 });
@@ -47,9 +30,4 @@ function pathPrefix(prefix) {
   return function(location) {
     return location.pathname.startsWith(`${prefix}`);
   };
-}
-
-// 热更新
-if (module.hot) {
-  module.hot.accept();
 }
